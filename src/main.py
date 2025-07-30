1dafb802f9b664060e59d9532ddd3e957557c806
Muhammad Ahmad <ahmadshaukat_4@outlook.com>
2025-07-30 19:24:14 +0500

    chore: comment out GalileoTracingProcessor initialization in main.py

--- src/main.py
+++ src/main.py
@@ -1,12 +1,12 @@
 import asyncio
 from agents import run_demo_loop, set_trace_processors
 from planner_agent import planner_supervisor_agent
-from galileo.handlers.openai_agents import GalileoTracingProcessor
+# from galileo.handlers.openai_agents import GalileoTracingProcessor
 
 
 
 async def main() -> None :
-  set_trace_processors([GalileoTracingProcessor()])
+  # set_trace_processors([GalileoTracingProcessor()])
   await run_demo_loop(planner_supervisor_agent)
 
 if __name__ == "__main__":
